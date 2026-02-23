6ec3bc31079adc33708f3db7ee904e2a98311e7f
ExBoyFriends <tomboy.maker@gmail.com>
2026-02-23 17:43:47 +0900

    Update card.js

--- card.js
+++ card.js
@@ -5,7 +5,7 @@ const back  = document.querySelector('.back');
 /* ======================
    CONFIG
 ====================== */
-const BASE_SPEED    = 2.4;
+const BASE_SPEED    = 0.4;
 const DRAG_SCALE    = 0.35;
 const DRAG_LIMIT    = 60;
 const INERTIA_DECAY = 0.92;
